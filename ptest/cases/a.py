ce0336ddfb80a6804606edabca7b5e5134875704
Shihao Cao <shihaocao@gmail.com>
2020-03-31 15:17:04 -0400

    aa

--- ptest/cases/a.py
+++ ptest/cases/a.py
@@ -10,15 +10,22 @@ class A(SingleSatOnlyCase):
         # self.sim.flight_controller.write_state("adcs_cmd.rwa_speed_cmd", "0, 0, 0") # 0 speed to begin with
         # self.sim.flight_controller.write_state("dcdc.ADCSMotor_cmd", "true")
 
+        self.ws("cycle.auto", True)
+
+        self.rs("gomspace.vbatt")
+
         self.ws("adcs_cmd.havt_reset7", True)
         self.ws("adcs_cmd.havt_reset8", True)
         self.ws("adcs_cmd.havt_reset9", True)
 
         self.ws("dcdc.ADCSMotor_cmd", True)
-        self.ws("adcs_cmd.rwa_speed_cmd", [50,50,50])
+        self.ws("adcs_cmd.rwa_speed_cmd", [10,10,10])
         self.ws("adcs_cmd.rwa_mode", self.rwa_modes.get_by_name("RWA_SPEED_CTRL"))
         self.ws("pan.state", self.mission_states.get_by_name("manual"))
         self.ws("adcs.state", self.adcs_states.get_by_name("point_manual"))
+
+        self.logger.put(self.rs("gomspace.vbatt"))
+        self.logger.put(self.rs("pan.cycle_no"))
 
         # time.sleep(5)
 
