b6079a4b79f9ed2d622e754d64d4cb4f2c632e6f
Shihao Cao <shihaocao@gmail.com>
2020-03-31 15:25:00 -0400

    bb

--- ptest/cases/b.py
+++ ptest/cases/b.py
@@ -15,10 +15,15 @@ class B(SingleSatOnlyCase):
         print(self.rs("gomspace.vbatt"))
 
         self.ws("dcdc.ADCSMotor_cmd", True)
+        print(self.rs("pan.cycle_no"))        
         self.ws("adcs_cmd.rwa_speed_cmd", [10,10,10])
+        print(self.rs("pan.cycle_no"))
         self.ws("adcs_cmd.rwa_mode", self.rwa_modes.get_by_name("RWA_SPEED_CTRL"))
+        print(self.rs("pan.cycle_no"))
         self.ws("pan.state", self.mission_states.get_by_name("manual"))
+        print(self.rs("pan.cycle_no"))
         self.ws("adcs.state", self.adcs_states.get_by_name("point_manual"))
+        print(self.rs("pan.cycle_no"))
 
         print(self.rs("gomspace.vbatt"))
         print(self.rs("pan.cycle_no"))
